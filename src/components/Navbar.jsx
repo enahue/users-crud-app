6e957615119b825b7502aba529ecbbc3fa857317
Erwin Nahue <erwinnahue@gmail.com>
2023-02-13 10:13:42 -0500

    update css styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,8 @@ const  Navbar = ({handleClickShowModal}) => {
  
   return (
     <nav className="navbar">
-      <h1 className="navbar__title">Users CRUD</h1>
-      <button className="navbar__btn" onClick={handleClickShowModal}><i className='bx bx-cross'></i> Create new user</button>
+      <h1 className="navbar__title">Users dashboard</h1>
+      <button className="navbar__btn" onClick={handleClickShowModal}><i className='bx bx-plus'></i> Create new user</button>
     </nav>
   )
 }
