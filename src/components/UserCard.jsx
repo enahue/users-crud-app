3346cc6ad3aa7830a7b4de36aa76d498b32ce8b0
Erwin Nahue <erwinnahue@gmail.com>
2023-02-16 13:24:01 -0500

    Fix button overflow in user card

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -28,17 +28,19 @@ const UserCard = ({
               <i className="bx bxs-gift userCard__icon"></i> {user.birthday}
             </li>
           </ul>
-        </div>
         <div className="userCard__flex-btn">
+          </div>
+          <div className="buttons-container">
           <button className="userCard__btn" onClick={handleClickEdit}>
             <i className="bx bx-pencil"></i>
           </button>
           <button
             className="userCard__btn red"
             onClick={() => deleteUser(user.id)}
-          >
+            >
             <i className="bx bx-trash"></i>
           </button>
+          </div>
         </div>
       </article>
     </div>
