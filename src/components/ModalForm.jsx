107493f8bffe987148f6828611077498e709badd
Erwin Nahue <erwinnahue@gmail.com>
2023-02-16 13:02:09 -0500

    Add fields validation implementation on form modal

--- src/components/ModalForm.jsx
+++ src/components/ModalForm.jsx
@@ -20,7 +20,7 @@ const ModalForm = ({
   setUpdatingUser
 }) => {
 
-  const {register, handleSubmit, reset} = useForm()
+  const {register, handleSubmit, reset, formState: {errors}} = useForm()
 
   const submit = (data) => {
     if(updatingUser){
@@ -38,6 +38,42 @@ const ModalForm = ({
     setUpdatingUser()
   }
 
+  //* Fields validation
+
+  //Patern regex email
+  const regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+  //Validations
+  const nameValidatedField = {
+    required: "* This field required",
+    maxLength: {
+      value: 25,
+      message: "Text is to long"
+  }};
+
+  const emailValidated = {
+    required: "* This field required",
+    maxLength: {
+      value: 150,
+      message: "Text is to long"
+  },
+  pattern: {
+    value: regexEmail,
+    message: "Not valid email address"
+  }
+  }
+
+  const passwordValidated = {
+    required: "* This field required",
+    maxLength: {
+      value: 128,
+      message: "Text is to long"
+  }};
+
+  const birthdayValidated = {
+    required: "* This field required",
+};
+
   useEffect(() => {
     if(updatingUser) {
       reset(updatingUser);
@@ -54,24 +90,39 @@ const ModalForm = ({
        <i className='bx bxs-user modalForm__icon'></i>
         <div className='modalForm__divname'>
           <label className='modalForm__label' htmlFor="">First Name</label>
-          <input className='modalForm__input' placeholder='First Name' type="text" {...register("first_name")} />
+          <input className='modalForm__input' placeholder='First Name' type="text" {...register("first_name",nameValidatedField)} />
+          {
+            errors.first_name && <p className='fields-validated'>{errors.first_name.message}</p>
+          }
         </div>
         <div className='modalForm__divname'>
           <label className='modalForm__label' htmlFor="">Last Name</label>
-          <input className='modalForm__input' placeholder='Last Name' type="text" {...register("last_name")}  />
+          <input className='modalForm__input' placeholder='Last Name' type="text" {...register("last_name", nameValidatedField)}  />
+          {
+            errors.last_name && <p className='fields-validated'>{errors.last_name.message}</p>
+          }
         </div>
         </div>
         <div className='modalForm__div row'>
           <label className='modalForm__label' htmlFor="">Email</label>
-          <i className='bx bxs-envelope modalForm__icon2'></i><input className='modalForm__input' placeholder='Email' type="email" {...register("email")}  />
+          <i className='bx bxs-envelope modalForm__icon2'></i><input className='modalForm__input' placeholder='Email' type="email" {...register("email", emailValidated)}  />
+          {
+            errors.email && <p className='fields-validated'>{errors.email.message}</p>
+          }
         </div>
         <div className='modalForm__div row'>
           <label className='modalForm__label' htmlFor="">Password</label>
-          <i className='bx bxs-lock modalForm__icon2'></i><input className='modalForm__input' placeholder='Password' type="password" {...register("password")} />
+          <i className='bx bxs-lock modalForm__icon2'></i><input className='modalForm__input' placeholder='Password' type="password" {...register("password", passwordValidated)} />
+          {
+            errors.password && <p className='fields-validated'>{errors.password.message}</p>
+          }
         </div>
         <div className='modalForm__div row'>
           <label className='modalForm__label' htmlFor="">Birthday</label>
-          <i className='bx bxs-gift modalForm__icon2'></i><input className='modalForm__input' type="date" {...register("birthday")} />
+          <i className='bx bxs-gift modalForm__icon2'></i><input className='modalForm__input' type="date" {...register("birthday", birthdayValidated)} />
+          {
+            errors.birthday && <p className='fields-validated'>{errors.birthday.message}</p>
+          }
         </div>
         <div className='modalForm__center'>
         <button className='modalForm__btn'>{updatingUser ? "Save changes" : "Add new user"}</button>
